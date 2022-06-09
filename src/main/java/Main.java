f0af9a1f8ba6faae496e22e6744a6ee9abd93609
Artur <ishmaev49@gmail.com>
2022-06-09 12:43:12 +0500

    r
    r

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,25 +12,31 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String pathToFilePrikrep = "D:\\files\\Prikrep.xlsx";
-        String pathToFilePrikrepZ = "D:\\files\\PrikrepZ.xlsx";
+        String pathToFilePrikrep = "D:\\files\\prikrep.xlsx";
+        String pathToFilePrikrepZ = "D:\\files\\p2c_7.xlsx";
         String result = "D:\\files\\result.xlsx";
-
-//        Workbook prikrep = new XSSFWorkbook(new FileInputStream(pathToFilePrikrep));
-//        Workbook prikrepZ = new XSSFWorkbook(new FileInputStream(pathToFilePrikrepZ));
+        String result2 = "D:\\files\\result2.xlsx";
 
         Workbook workbook1 = new XSSFWorkbook(new FileInputStream(pathToFilePrikrep));
         Workbook workbook2 = new XSSFWorkbook(new FileInputStream(pathToFilePrikrepZ));
 
-        Map<String, List<String>> dataFromExcel = sort(getDataFromExcel(workbook1));
-        Map<String, List<String>> dataToExcel = sort(getDataFromExcel(workbook2));
+        Map<String, String> maps = getDataFromExcel1(workbook1);
+        Map<String, String> maps2 = getDataFromExcel1(workbook2);
 
-        Map<String, List<String>> elems = findElems(dataFromExcel, dataToExcel);
-        elems.forEach((s, strings) -> System.out.println(s + ": " + strings));
-
-        Workbook wb = createWB(elems);
+//        Map<String, List<String>> dataFromExcel = getDataFromExcel(workbook1);
+//        Map<String, List<String>> dataToExcel = getDataFromExcel(workbook2);
+//
+        Map<String, String> elems = findElems1(maps2, maps);
+        Map<String, String> elems2 = findElems1(maps, maps2);
+//        elems.forEach((s, strings) -> System.out.println(s + ": " + strings));
+//
+        Workbook wb = createWB1(elems);
         wb.write(new FileOutputStream(result));
         wb.close();
+
+        Workbook wb1 = createWB1(elems2);
+        wb1.write(new FileOutputStream(result2));
+        wb1.close();
     }
 
     public static Workbook createWB(Map<String, List<String>> map) {
@@ -48,9 +54,32 @@ public class Main {
         return workbook;
     }
 
+    public static Workbook createWB1(Map<String, String> map) {
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet();
+        int i = 0;
+        for (Map.Entry<String, String> elem : map.entrySet()) {
+            Row row = sheet.createRow(i++);
+            row.createCell(0).setCellValue(elem.getKey());
+            row.createCell(1).setCellValue(elem.getValue());
+        }
+        return workbook;
+    }
+
+
     public static Map<String, List<String>> findElems(Map<String, List<String>> from, Map<String, List<String>> to) {
         Map<String, List<String>> newMap = new LinkedHashMap<>();
         for (Map.Entry<String, List<String>> elem : from.entrySet()) {
+            if (!to.containsKey(elem.getKey())) {
+                newMap.put(elem.getKey(), elem.getValue());
+            }
+        }
+        return newMap;
+    }
+
+    public static Map<String, String> findElems1(Map<String, String> from, Map<String, String> to) {
+        Map<String, String> newMap = new LinkedHashMap<>();
+        for (Map.Entry<String, String> elem : from.entrySet()) {
             if (!to.containsKey(elem.getKey())) {
                 newMap.put(elem.getKey(), elem.getValue());
             }
@@ -87,5 +116,27 @@ public class Main {
                         },
                         LinkedHashMap::new
                 ));
+    }
+
+    public static Map<String, String> getDataFromExcel1(Workbook workbook) {
+        Map<String, String> map = new HashMap<>();
+        Sheet sheet = workbook.getSheetAt(0);
+        for (Row row : sheet) {
+            StringBuilder key = new StringBuilder();
+            Iterator<Cell> iterator = row.iterator();
+            for (int i = 0; i < 4; i++) {
+                if (iterator.hasNext()) {
+                    key.append(iterator.next().getStringCellValue().trim().toLowerCase()).append(" ");
+                }
+            }
+            if (row.getCell(4) == null) {
+                map.put(key.toString(), "Null");
+            } else {
+                String value = row.getCell(4).getStringCellValue();
+                map.put(key.toString(), value);
+            }
+
+        }
+        return map;
     }
 }
